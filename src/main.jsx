1bff59aca99eecb9256538b46aec96a9a5149a68
Yashwanth2424 <thalkayashwanth1234@gmail.com>
2025-02-21 17:52:58 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+
+import ErrorPage from './Components/ErrorPage.jsx'
+import Translator from './Components/Translator.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    errorElement: <ErrorPage/>, 
+    children: [
+      { index: true, element: <Translator /> },
+     
+    ]
+    
+  }
+],
+{
+  basename: process.env.NODE_ENV === "production" ? "/Text-Translator-App" : "/"
+}
+)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
